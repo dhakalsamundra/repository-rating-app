7cb0a1b4a9833386e7e8ac688104e8ec4d6ba99b
dhakalsamundra <seapush02@gmail.com>
2020-10-04 08:51:43 +0300

    implement touchable

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text,View, StyleSheet } from 'react-native';
+import { Text,View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 import Constants from 'expo-constants';
 
 const styles = StyleSheet.create({
@@ -18,8 +18,10 @@ const styles = StyleSheet.create({
 
 const AppBar = () => {
   return <View style={styles.container}>
+      <TouchableWithoutFeedback>
           <Text>Samundra..</Text>
           
+       </TouchableWithoutFeedback>
        </View>;
 };
 
