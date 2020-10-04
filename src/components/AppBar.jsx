7e2bae3fff6ba7f1d27a6a13fd643a0cadd9765f
dhakalsamundra <seapush02@gmail.com>
2020-10-04 09:53:44 +0300

    add theme styling

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import { Text,View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 import Constants from 'expo-constants';
+import theme from '.././theme'
 
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: 'brown',
-    display:'flex'
-  },
-  flexItem: {
-    backgroundColor: 'blue',
-    flexGrow: 1
+// const styles = StyleSheet.create({
+//   container: {
+//     paddingTop: Constants.statusBarHeight,
+//     backgroundColor: 'brown',
+//     display:'flex'
+//   },
+//   flexItem: {
+//     backgroundColor: 'blue',
+//     flexGrow: 1
 
-  }
+//   }
   
-});
+// });
 
 const AppBar = () => {
-  return <View style={styles.container}>
+  return <View style={theme.appColor}>
       <TouchableWithoutFeedback>
           <Text>Samundra..</Text>
           
