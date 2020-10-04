badc4564def98cca0f8682a2052e10f152b10479
dhakalsamundra <seapush02@gmail.com>
2020-10-04 19:00:44 +0300

    --no-verify

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,27 +1,12 @@
 import React from 'react';
 import { Text,View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
-import Constants from 'expo-constants';
 import theme from '.././theme'
 
-// const styles = StyleSheet.create({
-//   container: {
-//     paddingTop: Constants.statusBarHeight,
-//     backgroundColor: 'brown',
-//     display:'flex'
-//   },
-//   flexItem: {
-//     backgroundColor: 'blue',
-//     flexGrow: 1
-
-//   }
-  
-// });
 
 const AppBar = () => {
   return <View style={theme.appColor}>
       <TouchableWithoutFeedback>
-          <Text>Samundra..</Text>
-          
+          <Text>Repositories</Text>          
        </TouchableWithoutFeedback>
        </View>;
 };
