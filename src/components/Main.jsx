dde92a5bc087600aba5d8ad17bf6c277af31bc5b
dhakalsamundra <seapush02@gmail.com>
2020-10-05 19:39:40 +0300

    add theme

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,10 +5,13 @@ import { Route, Switch, Redirect } from 'react-router-native';
 import RepositoryList from './RepositoryList';
 import SignIn from './SignIn'
 import AppBar from './AppBar';
+import PingIn from './PingIn';
+import theme from '../theme';
 
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: theme.colors.mainBackground,
     flexGrow: 1,
     flexShrink: 1,
     backgroundColor: '#e1e4e8'
@@ -20,7 +23,7 @@ const Main = () => {
     <View style={styles.container}>
       <StatusBar backgroundColor="black" barStyle="light-content"/>
       <AppBar />
-      {/* <Switch>
+      <Switch>
         <Route path="/" exact>
           <RepositoryList />
         </Route>
@@ -28,7 +31,7 @@ const Main = () => {
           <SignIn />
         </Route>
         <Redirect to="/" />
-      </Switch> */}
+      </Switch>
 
     </View>
   );
