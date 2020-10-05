b7e55a6e1c30a694e6acb67207d07a2fdc1158ea
dhakalsamundra <seapush02@gmail.com>
2020-10-05 19:39:57 +0300

    add tabs

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,37 +1,35 @@
 import React from 'react';
-import { View,Text, TouchableWithoutFeedback,StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
 import { Link } from "react-router-native";
-
+import { View, StyleSheet, Text, ScrollView } from 'react-native';
+import Constants from 'expo-constants';
 const styles = StyleSheet.create({
-  container: {
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#24292e',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    // borderWidth: 2,
-    // borderColor: 'red',
-  },
-  link: {
-    color: 'white',
-    // borderWidth: 2,
-    // borderColor: 'red',
-  }
-});
-
-const AppBar = () => {
-  return (
-  <View style={styles.container}>
-    <View style={styles.link}><TouchableWithoutFeedback>
-    <Link to="/"><Text>Repositories</Text></Link></TouchableWithoutFeedback>
-    </View>
-    <View style={styles.link}>
-    <TouchableWithoutFeedback>
-    <Link to="/signin"><Text>Sign in</Text></Link>
-    </TouchableWithoutFeedback></View>
-  </View>
-  );
-};
-
-export default AppBar;
+    container: {
+      paddingTop: Constants.statusBarHeight,
+      // ...
+      backgroundColor: '#24292e',
+      display: "flex",
+      flexDirection: "row"
+    },
+    textItem: {
+        color: '#ffffff',
+        margin: 20
+    }
+    // ...
+  });
+  
+  const AppBar = () => {
+    return (
+              <View style={styles.container}>
+                <ScrollView horizontal={true}>
+                  <Link to="/">
+                    <Text style={styles.textItem}>Repositories</Text>
+                  </Link>
+                  <Link to="/signin">
+                    <Text style={styles.textItem}>Sign In</Text>
+                  </Link>
+                </ScrollView>
+              </View>
+            );
+  };
+  
+  export default AppBar;
