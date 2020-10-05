0defeac88569fcd5b64bc3a00aee0d519b360f82
dhakalsamundra <seapush02@gmail.com>
2020-10-05 11:07:25 +0300

    create screen tab

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { Text,View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 import theme from '.././theme'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+import RepositoryList from './RepositoryList';
+import SignIn from './SignIn'
+
+const Tab = createMaterialTopTabNavigator();
 
 
 const AppBar = () => {
-  return <View style={theme.appColor}>
-      <TouchableWithoutFeedback>
-          <Text>Repositories</Text>          
-       </TouchableWithoutFeedback>
-       </View>;
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+      <Tab.Screen name="Repositories" component={RepositoryList} />
+      <Tab.Screen name="Sign in" component={SignIn} />
+    </Tab.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default AppBar;
