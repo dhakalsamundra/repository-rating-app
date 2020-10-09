8aad04552df4ae672100b5b924b2092fb8c48373
dhakalsamundra <seapush02@gmail.com>
2020-10-09 20:02:40 +0300

    add graphql queries

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -2,10 +2,12 @@ import { gql } from 'apollo-boost';
 
 export const GET_REPOSITORIES = gql`
   query repositories {
-      fullName,
-      description,
-      language,
-      forksCount,
-      stargezersCount
-  }
+    fullName,
+    ownerAvatarUrl,
+    description,
+    language,
+    forksCount,
+    ratingAverage,
+    reviewCount,
+    stargazersCount
 `;
