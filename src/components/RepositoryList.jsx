0b90f4116b5bde1395f95000d7cd89eb4b3b4ff4
dhakalsamundra <seapush02@gmail.com>
2020-10-11 11:51:19 +0300

    add query data

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
 import { FlatList, View, StyleSheet } from 'react-native';
-
+import React from 'react';
 import useRepositories from '../hooks/useRepositories';
 
 import RepositoryItem from './RepositoryItem';
@@ -14,14 +13,21 @@ const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryList = () => {
   const { repositories } = useRepositories();
-
+  console.log('I am samundra')
    // Get the nodes from the edges array
    const repositoryNodes = repositories
    ? repositories.edges.map(edge => edge.node)
    : [];
+   console.log()
+
 
 
   return (
+    (loading, error, data) => {
+      if (loading) return <h4>Loading...</h4>;
+      if (error) console.log(error);
+      return data
+    },
     <FlatList
       data={repositoryNodes}
       ItemSeparatorComponent={ItemSeparator}
